2259aab7aaa0dba5bf0863e8bbecceae55f1fd3b
Kravets Pavel <kravec42@gmail.com>
2025-09-24 17:04:47 -0500

    Initial Commit

---
+++ AISpotlight/AISpotlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  AISpotlightApp.swift
+//  AISpotlight
+//
+//  Created by Pavel Kravets on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct AISpotlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
