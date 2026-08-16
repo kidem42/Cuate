276368cb7f09a2bd5090593a1df59095d52d7ef8
Kravets Pavel <kravec42@gmail.com>
2026-08-15 23:22:58 -0500

    Release 4.11 · Android 2.8.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.aispotlight.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 26
-        versionName = "2.7.0"
+        versionCode = 27
+        versionName = "2.8.0"
     }
 
     signingConfigs {
