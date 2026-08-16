dedf858c8ccb1f364bb4e77a9add27aa5b8a43b7
Kravets Pavel <kravec42@gmail.com>
2026-08-16 13:05:01 -0500

    Release: Cuate 4.12, Android 2.8.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.aispotlight.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 27
-        versionName = "2.8.0"
+        versionCode = 28
+        versionName = "2.8.1"
     }
 
     signingConfigs {
